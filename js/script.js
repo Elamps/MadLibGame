3d37e746c8811aea3e1806004e8bf8410bc16648
Marcus Lampley <Marcus@Kathleens-Air.hsd1.ga.comcast.net>
2020-08-10 11:28:21 -0400

    Updated input fields

--- js/script.js
+++ js/script.js
@@ -1,21 +1,60 @@
-let blanks = [];
+    // "title": "Talk Like a Pirate",
+    // "blanks": [
+    //     "noun",
+    //     "adjective",
+    //     "verb",
+    //     "adverb",
+    //     "noun",
+    //     "adjective",
+    //     "plural noun",
+    //     "plural noun",
+    //     "plural noun",
+    //     "part of the body",
+    //     "noun",
+    //     "noun",
+    //     "noun",
+    //     "noun",
+    //     "part of the body"
 
-const startButton = document.getElementById("startButton");
 
 
-startButton.addEventListener('click', ()=> {
-    fetch("http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=30")
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data)
-        // let newLib = {}
-        // newLib["blanks"] = data.blanks
-        // blanks.push(newLib)
-        // render()
-    })
-    // console.log(blanks)
-    // .catch((err) => {
-    // console.log(err)
-})
+
+    // "value": 
+    //     "Ye can always pretend to be a bloodthirsty ",
+    //     ", threatening everyone by waving yer ",
+    //     " sword in the air, but until ye learn to ",
+    //     " like a pirate, ye'll never be ",
+    //     " accepted as an authentic ",
+    //     ". So here's what ye do: Cleverly work into yer daily conversations ",
+    //     " pirate phrases such as \"Ahoy there, ",
+    //     ",\"\"Avast, ye ",
+    //     ",\" and \"Shiver me ",
+    //     ".\" Remember to drop all yer gs when ye say such words as sailin', spittin', and fightin'. This will give ye a/an ",
+    //     " start to being recognized as a swashbucklin' ",
+    //     ". Once ye have the lingo down pat, it helps to wear a three-cornered ",
+    //     " on yer head, stash a/an ",
+    //     " in yer pants, and keep a/an ",
+    //     " perched atop yer ",
+    //     ". Aye, now ye be a real pirate!",
+        
+// let blanks = [];
+
+// const startButton = document.getElementById("startButton");
+
+
+// startButton.addEventListener('click', ()=> {
+//     fetch("http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=30")
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then((data) => {
+//         console.log(data)
+//         // let newLib = {}
+//         // newLib["blanks"] = data.blanks
+//         // blanks.push(newLib)
+//         // render()
+//     })
+//     // console.log(blanks)
+//     // .catch((err) => {
+//     // console.log(err)
+// })
