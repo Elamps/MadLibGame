7beb618367c165e10184476fead63247bb6b244b
Marcus Lampley <Marcus@Kathleens-Air.hsd1.ga.comcast.net>
2020-08-10 12:54:15 -0400

    add story and var inputs in js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-    // "title": "Talk Like a Pirate",
+let noun1 = document.querySelector('#noun1')
+let adj1 = document.querySelector('#adj1')
+let verb1 = document.querySelector('#verb1')
+let adv1 = document.querySelector('#adv1')
+let noun2 = document.querySelector('#noun2')
+let adj2 = document.querySelector('#adj2')
+let pnoun1 = document.querySelector('#pnoun1')
+let pnoun2 = document.querySelector('#pnoun2')
+let pnoun3 = document.querySelector('#pnoun3')
+let pob1 = document.querySelector('#pob1')
+let noun3 = document.querySelector('#noun3')
+let noun4 = document.querySelector('#noun4')
+let noun5 = document.querySelector('#noun5')
+let noun6 = document.querySelector('#noun6')
+let pob2 = document.querySelector('#pob2')
+
+button.addEventListener.writeLib('click');
+
+function writeLib() {
+    document.write("Ye can always pretend to be a bloodthirsty " + noun1 + ", threatening everyone by waving yer " + adj1 + " sword in the air, but until ye learn to " + verb1 + " like a pirate, ye'll never be " + adv1 + " accepted as an authentic " + noun2 + ". So here's what ye do: Cleverly work into yer daily conversations " + adj2 + " pirate phrases such as \"Ahoy there, " + pnoun1 + ",\"\"Avast, ye " + pnoun2 + ",\" and \"Shiver me " + pnoun3 + ".\" Remember to drop all yer gs when ye say such words as sailin', spittin', and fightin'. This will give ye a/an " + pob1 + " start to being recognized as a swashbucklin' " + noun3 + ". Once ye have the lingo down pat, it helps to wear a three-cornered " + noun4 + " on yer head, stash a/an " + noun5 +" in yer pants, and keep a/an " + noun6 + " perched atop yer " + pob2 +". Aye, now ye be a real pirate!",);
+}
+
+// "title": "Talk Like a Pirate",
     // "blanks": [
     //     "noun",
     //     "adjective",
