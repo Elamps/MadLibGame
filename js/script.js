c168d349daaa4514cc35127028d9622e12bb8a66
Marcus Lampley <Marcus@Kathleens-Air.hsd1.ga.comcast.net>
2020-08-10 13:10:23 -0400

    working mad lib

--- js/script.js
+++ js/script.js
@@ -13,11 +13,14 @@ let noun4 = document.querySelector('#noun4')
 let noun5 = document.querySelector('#noun5')
 let noun6 = document.querySelector('#noun6')
 let pob2 = document.querySelector('#pob2')
+let start = document.querySelector('#start')
 
-button.addEventListener.writeLib('click');
+start.addEventListener('click', writeLib, false)
+
 
 function writeLib() {
-    document.write("Ye can always pretend to be a bloodthirsty " + noun1 + ", threatening everyone by waving yer " + adj1 + " sword in the air, but until ye learn to " + verb1 + " like a pirate, ye'll never be " + adv1 + " accepted as an authentic " + noun2 + ". So here's what ye do: Cleverly work into yer daily conversations " + adj2 + " pirate phrases such as \"Ahoy there, " + pnoun1 + ",\"\"Avast, ye " + pnoun2 + ",\" and \"Shiver me " + pnoun3 + ".\" Remember to drop all yer gs when ye say such words as sailin', spittin', and fightin'. This will give ye a/an " + pob1 + " start to being recognized as a swashbucklin' " + noun3 + ". Once ye have the lingo down pat, it helps to wear a three-cornered " + noun4 + " on yer head, stash a/an " + noun5 +" in yer pants, and keep a/an " + noun6 + " perched atop yer " + pob2 +". Aye, now ye be a real pirate!",);
+    console.log("write")
+    document.write("Ye can always pretend to be a bloodthirsty " + noun1.value + ", threatening everyone by waving yer " + adj1.value  + " sword in the air, but until ye learn to " + verb1.value  + " like a pirate, ye'll never be " + adv1.value  + " accepted as an authentic " + noun2.value  + ". So here's what ye do: Cleverly work into yer daily conversations " + adj2.value  + " pirate phrases such as \"Ahoy there, " + pnoun1.value  + ",\"\"Avast, ye " + pnoun2.value  + ",\" and \"Shiver me " + pnoun3.value  + ".\" Remember to drop all yer gs when ye say such words as sailin', spittin', and fightin'. This will give ye a/an " + pob1.value  + " start to being recognized as a swashbucklin' " + noun3.value  + ". Once ye have the lingo down pat, it helps to wear a three-cornered " + noun4.value  + " on yer head, stash a/an " + noun5.value  +" in yer pants, and keep a/an " + noun6.value  + " perched atop yer " + pob2.value  +". Aye, now ye be a real pirate!",);
 }
 
 // "title": "Talk Like a Pirate",
